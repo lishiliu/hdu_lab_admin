1e2d2f39a45e44c37b337ca6937cb37f2fd3ad37
lishiliu <liyanzhaohyh@163.com>
2019-06-19 04:43:14 +0800

    编辑设备

--- src/app/routes/computer/editComputer/editComputer.component.ts
+++ src/app/routes/computer/editComputer/editComputer.component.ts
@@ -18,92 +18,24 @@ export class EditComputerComponent implements OnInit {
     loadStatus: boolean;
     submitBtn = '提交';
     curl = [
-        'class/updateclass', // 0编辑课程
-        'semester/getNowSemester' // 1获取当前学期
+        'computer/updateComputer', // 0 编辑课程
+        'semester/getNowSemester', // 1获取当前学期
+        'lab/getLabById', /*2 获取实验室*/
     ];
     constructor(private _storage: SessionStorageService, private fb: FormBuilder, private router: Router,
-                private editcourseService: EditComputerService, private confirmServ: NzModalService) {
-    }
-    week = [];
-    weekday = [{ value: 1, label: '星期一' },
-        { value: 2, label: '星期二' },
-        { value: 3, label: '星期三' },
-        { value: 4, label: '星期四' },
-        { value: 5, label: '星期五' },
-        { value: 6, label: '星期六' },
-        { value: 7, label: '星期日' }
-    ];
-    classNum = [{ value: 1, label: '第1节' },
-        { value: 2, label: '第2节' },
-        { value: 3, label: '第3节' },
-        { value: 4, label: '第4节' },
-        { value: 5, label: '第5节' },
-        { value: 6, label: '第6节' },
-        { value: 7, label: '第7节' },
-        { value: 8, label: '第8节' },
-        { value: 9, label: '第9节' },
-        { value: 10, label: '第10节' },
-        { value: 11, label: '第11节' },
-        { value: 12, label: '第12节' },
-    ];
-    _course;
-    private setInitWeek() {
-        for (let i = 0; i < 30; i++) {
-            this.week[i] = {value : i + 1, label: (i + 1).toString()};
-        }
-        console.log(this.week);
+                private editComputerService: EditComputerService, private confirmServ: NzModalService) {
     }
     nowSemester = {
         nowSemester: '',
         maxWeek: 17
     };
-    private _getData = () => {
-        this.setInitWeek();
-        // 获取学期
-        this.editcourseService.executeGET(this.curl[1])
-            .then((result: any) => {
-                const res = JSON.parse(result['_body']);
-                if (res['result'] === 'success') {
-                    this.nowSemester = res['NowSemester'];
-                }
-            });
-        // 获取课程c
-        this._course = JSON.parse(this._storage.get('course'));
-    }
-    _back() {
-        window.history.back();
-    }
-    /*//控制全选单双重置*/
-    setWeek = (target, operation) => {
-        this.validateForm.controls[target].reset();
-        if (operation == 0) {
-            let c = this.validateForm.value;
-            c.week = this.week;
-            this.validateForm.setValue(c);
-        }
-        if(operation==1){
-            let c = this.validateForm.value;
-            c.week = [];
-            for(let i=0;i< this.week.length;i++){
-                if(i%2==0){
-                    c.week.push(this.week[i]);
-                }
-            }
-            this.validateForm.setValue(c);
-        }
-        if(operation==2){
-            let c = this.validateForm.value;
-            c.week = [];
-            for(let i=0;i< this.week.length;i++){
-                if(i%2){
-                    c.week.push(this.week[i]);
-                }
-            }
-            this.validateForm.setValue(c);
-        }
-    };
-    //控制全选单双重置
-
+    labName;
+    computerInfo;
+    /* computerIp;
+     computerNum;
+     software;
+     systemType;
+     computerType;*/
     getFormControl(name) {
         return this.validateForm.controls[name];
     }
@@ -116,47 +48,63 @@ export class EditComputerComponent implements OnInit {
     }
     success = () => {
         const modal = this.confirmServ.success({
-            title: '修改成功',
+            title: '编辑成功',
             content: '1秒后回到课程管理'
         });
-        this._storage.remove('course');
+        this._storage.remove('computer');
         const Route = this.router;
         setTimeout(function () {
             modal.destroy();
-            Route.navigate(['/courses']);
+            Route.navigate(['/computer']);
         }, 1000);
     }
     _submitForm() {
         let data = {
-            id: this._course.id,
-            userName: this._storage.get('username'),
-            classId: this._course.classId,
-            className: this._course.className, // 课程
-            classWeek: this._course.classWeek, // 周数
-            weekDays: this._course.weekDays, // 星期几
-            classNum: this._course.classNum, // 第几节
-            classPeoCount: this._course.classPeoCount
+            id: this.computerInfo.id,
+            computerIp: this.computerInfo.computerIp, // IP
+            computerNum: this.computerInfo.computerNum, // 座号
+            software: this.computerInfo.software, // 软件
+            systemType: this.computerInfo.systemType, // 操作系统类型
+            type: this.computerInfo.type // 设备类型
         };
-        this.editcourseService.executeHttp(this.curl[0], data)
+        this.editComputerService.executeHttp(this.curl[0], data)
             .then((result: any) => {
-                let res = JSON.parse(result['_body']);
-                if (res['result'] === 1) {
+                const res = JSON.parse(result['_body']);
+                if (res['result'] === 'success') {
                     this.success();
+                } else if (res['result'] === 'repeat') {
+                    this.info('警告', res['msg'] );
                 } else {
                     this.info('警告', '修改失败,请检查后重试！');
                     return;
                 }
             });
     }
+    private getData() {
+        // 获取学期
+        this.editComputerService.executeGET(this.curl[1])
+            .then((result: any) => {
+                const res = JSON.parse(result['_body']);
+                if (res['result'] === 'success') {
+                    this.nowSemester = res['NowSemester'];
+                }
+            });
+        // 获取实验室
+        this.editComputerService.executeHttp(this.curl[2], {labId: this._storage.get('labId')})
+            .then((result: any) => {
+                this.labName = JSON.parse(result['_body'])['lab']['labName'];
+            });
+        // 获取设备
+        this.computerInfo = JSON.parse(this._storage.get('computer'));
+    }
     ngOnInit() {
-        this._getData();
+        this.getData();
         this.validateForm = this.fb.group({
-            classId: [this._course.classId, [Validators.required]],
-            className: [this._course.className, [Validators.required]],
-            week: [this._course.classWeek, [Validators.required]],
-            weekday: [this._course.weekDays[0], [Validators.required]],
-            classNum: [this._course.classNum, [Validators.required]],
-            classPeoCount: [this._course.classPeoCount, [Validators.required]],
+            computerIp: [this.computerInfo.computerIp, [Validators.required]],
+            computerNum: [this.computerInfo.computerNum, [Validators.required]],
+            software: [this.computerInfo.software, [Validators.required]],
+            systemType: [this.computerInfo.systemType, [Validators.required]],
+            computerType: [this.computerInfo.type, [Validators.required]],
         });
     }
 }
